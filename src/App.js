9a20ddfb2f26642dbe9467613a732cefcef8a3bd
unknown <hafsabinteharun101@gmail.com>
2022-12-20 16:56:00 +0600

    eight

--- src/App.js
+++ src/App.js
@@ -44,7 +44,10 @@ function App() {
           element: <PrivateRoutes><Checkout></Checkout></PrivateRoutes>,
           loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
         },
-       
+        {
+          path: '/review',
+          element: <Review></Review>
+        }
       ]
     }
   ])
